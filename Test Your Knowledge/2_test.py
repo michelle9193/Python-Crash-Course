6985c12394b22e5620be6d308e339d721577aca0
michelle9193 <michellefortuin93@gmail.com>
2024-08-08 11:49:16 +0200

    updating folder structure

--- Test Your Knowledge/2_test.py
+++ Test Your Knowledge/2_test.py
@@ -30,6 +30,54 @@
 # message = f'\t{famous_person.title()} once said, "{quote}"'
 # print(message)
 
-# 2-7 Stripping Names
+# # 2-7 Stripping Names
 # name = ' \nlucas\t  '
+# print(name.lstrip())
+# print(name.rstrip())
 # print(name.strip())
+
+# # 2-8 File Extensions
+# filename = 'python_notes.txt'
+# test = filename.removesuffix('.txt')
+# print(test)
+
+# # 2-9 Number Eight
+# # addition
+# print(6+2)
+# # multiplication
+# print(2*4)
+# # subtraction
+# print(18-10)
+# # division
+# print(16/2)
+
+# # 2-10 Favorite Number
+# favorite_number = 10
+# print(f"My favorite number is {favorite_number}")
+
+# 2-11 Adding Comments
+# Name: Patty
+# Date: 8/8/2024
+
+# 2-12 Zen of Python
+# Enter import this in terminal
+# RESULT:
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
--- favorite_number.py
+++
@@ -1,2 +0,0 @@
-favorite_number = 10
-print(f"My favorite number is {favorite_number}")
