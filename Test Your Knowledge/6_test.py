e126330289f1ac38b1f8b663c50e1f7c66fe45ec
michelle9193 <michellefortuin93@gmail.com>
2024-08-09 08:47:15 +0200

    add files to folder

--- Test Your Knowledge/6_test.py
+++ Test Your Knowledge/6_test.py
@@ -88,7 +88,86 @@ for name in friends:
     if name not in favorite_languages:
         print(f"\t{name.title()}, please take our poll!")
 
-    
+# 6-7 People
+# person_0 = {'first_name': 'lindsay', 'last_name': 'brown', 'age': 28, 'city': 
+#             "new york"}
+# person_1 = {'first_name': 'tim', 'last_name': 'lopez', 'age': 22, 'city': 
+#             "california"}
+# person_2 = {'first_name': 'nelly', 'last_name': 'smith', 'age': 26, 'city': 
+#             "hamptons"}
+
+# people = [person_0, person_1, person_2]
+
+# for people in people:
+#     print(people)
+
+# 6-8 Pets
+# vicky = {'type': 'tortoise', 'owner': 'wendy harper'}
+# todd = {'type': 'dog', 'owner': 'holly mcintyre'}
+# keith = {'type': 'cat', 'owner': 'spencer mallow'}
+
+# pets = [vicky, todd, keith]
+
+# for pet in pets:
+#     print(f'{pet}')
+
+# 6-9 Favorite Places
+# favorite_places = {
+#     'harper': ['bali', 'switzerland'],
+#     'james': ['singapore', 'germany'],
+#     'lenny': ['paris', 'london'],
+#     'josh': ['new zealand', 'italy']
+#     }
+
+# for name, places in favorite_places.items():
+#     print(f"{name.title()} favorite places are:")
+#     for place in places:
+#         print(f"\t{place.title()}")
+
+# 6-10 Favorite Numbers
+# favorite_numbers = {
+#     'brandy': [13, 5, 33], 
+#     'marshall': [22, 11], 
+#     'stephanie': [16, 30, 2], 
+#     'kelly': [92, 14, 88],
+#     'kimberley': [69, 26 ,1]
+#     }
+
+# for name, numbers in favorite_numbers.items():
+#     print(f"\n{name.title()} favorite numbers are:")
+#     for number in numbers:
+#         print(f"{number}")
+
+# 6-11
+# cities = {
+#     'cape town' : {
+#         'country': 'south africa',
+#         'population': 4618000,
+#         'fact': 'Table Mountain is one of the New 7 Wonders of Nature.',
+#         },
+
+#     'yishun' : {
+#         'country': 'singapore',
+#         'population': 240000,
+#         'fact': ' The worlds first nocturnal zoo-cum- safari is located here.'
+# ,
+#         },
+
+#     'mauritius' : {
+#         'country': 'east africa',
+#         'population': 1263000,
+#         'fact': 'the only African nation with Hinduism as the dominant 
+# religion.',
+#         }   
+#     }
+
+# for name, city_info in cities.items():
+#     print(f"\n{name.title()}")
+#     print(f"\tCountry: {city_info['country'].title()}")
+#     print(f"\tApproximate Population: {city_info['population']}")
+#     print(f"\tFun Fact: {city_info['fact'].title()}")
+
+# 6-12 Extensions   
 
 
 
