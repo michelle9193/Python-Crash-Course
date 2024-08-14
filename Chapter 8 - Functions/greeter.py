31cc21dc10592bab69ac99c5f61c4614269b7641
michelle9193 <michellefortuin93@gmail.com>
2024-08-14 08:48:03 +0200

    update

---
+++ Chapter 8 - Functions/greeter.py
@@ -0,0 +1,13 @@
+# # Defining a function
+# def greet_user():
+#     """Display a simple greeting."""
+#     print("Hello!")
+
+# greet_user()
+
+# Passing Information to a Function
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+greet_user('jesse')
