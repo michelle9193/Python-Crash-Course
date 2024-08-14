1d066380f8cc75ac3a0ec0b489b9791d597d9989
michelle9193 <michellefortuin93@gmail.com>
2024-08-14 08:53:59 +0200

    add try it yourself 8-1 and 8-2

---
+++ Try It Yourself/8_test.py
@@ -0,0 +1,13 @@
+# # 8-1 Message 
+# def display_message():
+#     """What I am learning in this chapter"""
+#     print("I am learning about functions in chapter 8.")
+
+# display_message()
+
+# # 8-2 Favorite Book
+# def favorite_book(title):
+#     """One of my favorite books"""
+#     print(f"One of my favorite books is {title.title()}!")
+
+# favorite_book("python crash course")
