ba6715e8a6b01104f4efeff8ba84b902ebb81873
michelle9193 <michellefortuin93@gmail.com>
2024-08-15 09:59:27 +0200

    update

---
+++ Chapter 8 - Functions/pets.py
@@ -0,0 +1,40 @@
+# # Positional Arguments
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet('hamster', 'harry')
+
+# # Multiple Function Calls
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet('hamster', 'harry')
+# describe_pet('dog', 'willie')
+
+# # Order matters in positional arguments
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet('harry', 'hamster')
+
+# # Keyword arguments
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet(animal_type='hamster', pet_name='harry')
+
+# # Default Values
+# def describe_pet(pet_name, animal_type="dog"):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet(pet_name='willie')
