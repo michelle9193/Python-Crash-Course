9abf53d4b4e47c8d911033f0eed1a91f01aedc75
michelle9193 <michellefortuin93@gmail.com>
2024-08-17 23:05:15 +0200

    update

--- Chapter 8 - Functions/printing_models.py
+++ Chapter 8 - Functions/printing_models.py
@@ -1,16 +1,43 @@
-# Modifying a list in a function
-# Start with some designs that need to be printed.
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
+# # Modifying a list in a function
+# # Start with some designs that need to be printed.
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
 
-# Simulate printing each design, until none are left.
-# Move each design to completed_models after printing.
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
+# # Simulate printing each design, until none are left.
+# # Move each design to completed_models after printing.
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
 
-# Display all completed models.
-print("\nThe following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
+# # Display all completed models.
+# print("\nThe following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+
+# Reorganize the code by writing two functions, each of which does one specific job
+# The first function will handle printing the designs, and the second will 
+# summarize the prints that been made.
+# def print_models(unprinted_designs, completed_models):  
+#     """
+#     Simulate printing each design, until none are left.
+#     Move each design to completed_models after printing.
+#     """
+#     while unprinted_designs:
+#         current_design = unprinted_designs.pop()
+#         print(f"Printing model: {current_design}")
+#         completed_models.append(current_design)
+
+# def show_completed_models(completed_models):
+#     """Show all the models that were printed."""
+#     print("\nThe following models have been printed:")
+#     for completed_model in completed_models:
+#         print(completed_model)
+    
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
+
+# Preventing a function from modifying a list
