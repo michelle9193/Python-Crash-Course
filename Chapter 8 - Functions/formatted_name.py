556d0caaf69f55ec1190852faaf7ff5b654ed724
michelle9193 <michellefortuin93@gmail.com>
2024-08-17 20:02:07 +0200

    add new file

---
+++ Chapter 8 - Functions/formatted_name.py
@@ -0,0 +1,10 @@
+# # Returning a Simple Value
+# def get_foramtted_name(first_name, last_name):
+#     """Return full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# musician = get_foramtted_name('jimi', 'hendrix')
+# print(musician)
+
+# Making an Argument Optional
