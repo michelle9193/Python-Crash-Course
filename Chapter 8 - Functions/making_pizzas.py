d306f4a7d8ba49bda7b1601049909c807e476e85
michelle9193 <michellefortuin93@gmail.com>
2024-08-19 15:46:23 +0200

    add file

---
+++ Chapter 8 - Functions/making_pizzas.py
@@ -0,0 +1,19 @@
+# Importing an entire module
+# import pizza
+
+# pizza.make_pizza(16, 'pepperoni')
+# pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# Importing specific functions
+# from pizza import make_pizza
+
+# make_pizza(16, 'pepperoni')
+# make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# Using as to give a function an alias
+# from pizza import make_pizza as mp
+
+# mp(16, 'pepperoni')
+# mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# Using as to give a module an alias
