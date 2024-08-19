02ab53b53bfbd90e77b3a9e261039925ab003398
michelle9193 <michellefortuin93@gmail.com>
2024-08-19 21:23:12 +0200

    add file

--- Chapter 9 - Classes/dog.py
+++ Chapter 9 - Classes/dog.py
@@ -29,3 +29,12 @@ my_dog.sit()
 my_dog.roll_over()
 
 # Creating Multiple Instances
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
