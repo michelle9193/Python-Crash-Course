67918eef29e6b37435aa7f1663191bc52d369b67
michelle9193 <michellefortuin93@gmail.com>
2024-08-20 13:05:49 +0200

    add 9-2 exercise

--- Try It Yourself/9_test.py
+++ Try It Yourself/9_test.py
@@ -1,27 +1,55 @@
-# 9-1 Restaurant
-class Restaurant:
-    """Model a restaurant"""
+# # 9-1 Restaurant
+# class Restaurant:
+#     """Model a restaurant"""
 
-    def __init__(self, restaurant_name, cuisine_type):
-        "Initialize name and cuisine type attributes"
-        self.name = restaurant_name.title()
-        self.type = cuisine_type
+#     def __init__(self, restaurant_name, cuisine_type):
+#         "Initialize name and cuisine type attributes"
+#         self.name = restaurant_name.title()
+#         self.type = cuisine_type
 
-    def describe_restaurant(self):
-        """Describe a restaurant"""
-        print(f"{self.name} has fresh flowers on display.")
-        print(f"{self.name} ships in fresh lobster daily.")
+#     def describe_restaurant(self):
+#         """Describe a restaurant"""
+#         message = f"{self.name} serves the best {self.type}."
+#         print(f"\n{message}")
 
-    def open_restaurant(self):
-        """Show the working hours of a restaurant"""
-        print(f"{self.name} is now open!")
+#     def open_restaurant(self):
+#         """Show the working hours of a restaurant"""
+#         message = f"{self.name} is open til late!"
+#         print(f"\n{message}")
 
-# Make an instance called restaurant from the class
-the_restaurant = Restaurant('spur', 'steak')
+# # Make an instance called restaurant from the class
+# restaurant = Restaurant('spur', 'steak')
+# print(restaurant.name)
+# print(restaurant.type)
 
+# # Calling methods
+# restaurant.describe_restaurant()
+# restaurant.open_restaurant()
 
-print(f"They sell the best {the_restaurant.type}.")
+# # 9-Three Restaurants
+# class Restaurant:
+#     """Model a restaurant"""
 
-# Calling methods
-the_restaurant.describe_restaurant()
-the_restaurant.open_restaurant()
+#     def __init__(self, restaurant_name, cuisine_type):
+#         "Initialize name and cuisine type attributes"
+#         self.name = restaurant_name.title()
+#         self.type = cuisine_type
+
+#     def describe_restaurant(self):
+#         """Describe a restaurant"""
+#         message = f"{self.name} serves the best {self.type}."
+#         print(f"\n{message}")
+
+#     def open_restaurant(self):
+#         """Show the working hours of a restaurant"""
+#         print(f"{self.name} is now open!")
+
+# # Make an instance called restaurant from the class
+# spur = Restaurant('spur', 'steak')
+# spur.describe_restaurant()
+
+# mcds = Restaurant('mcdonalds', 'burger')
+# mcds.describe_restaurant()
+
+# kfc = Restaurant('kfc', 'fried chicken')
+# kfc.describe_restaurant()
