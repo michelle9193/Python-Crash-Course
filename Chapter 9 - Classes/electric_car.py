49430cd6f0880a5e1534a7ad085b8aef9993297c
michelle9193 <michellefortuin93@gmail.com>
2024-08-22 09:55:02 +0200

    update

--- Chapter 9 - Classes/electric_car.py
+++ Chapter 9 - Classes/electric_car.py
@@ -38,6 +38,14 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 15
+        elif self.battery_size == 65:
+            range = 225
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
@@ -60,3 +68,4 @@ class ElectricCar(Car):
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
