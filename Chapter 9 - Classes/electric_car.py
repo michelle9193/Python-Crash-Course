93b69680039badc09e56634e35b453b1d7d7688e
michelle9193 <michellefortuin93@gmail.com>
2024-08-22 09:43:19 +0200

    update

--- Chapter 9 - Classes/electric_car.py
+++ Chapter 9 - Classes/electric_car.py
@@ -41,6 +41,11 @@ class ElectricCar(Car):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    # Overriding Methods form the Parent Class
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't have a gas tank!")
+
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
