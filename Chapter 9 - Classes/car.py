755f9a14ed53f5e8331e80c5f02e47635f54c957
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:30:57 +0200

    update

--- Chapter 9 - Classes/car.py
+++ Chapter 9 - Classes/car.py
@@ -46,56 +46,56 @@
 
 # Modifying Attribute Values
 # Modifying an Attribute's Value Directly
-class Car:
-    """A simple attempt to represent a car."""
+# class Car:
+#     """A simple attempt to represent a car."""
     
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+#     def __init__(self, make, model, year):
+#         """Initialize attributes to describe a car."""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
     
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = f"{self.year} {self.make} {self.model}"
+#     def get_descriptive_name(self):
+#         """Return a neatly formatted descriptive name."""
+#         long_name = f"{self.year} {self.make} {self.model}"
 
-        return long_name.title()
+#         return long_name.title()
     
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print(f"This car has {self.odometer_reading} miles on it.")
+#     def read_odometer(self):
+#         """Print a statement showing the car's mileage."""
+#         print(f"This car has {self.odometer_reading} miles on it.")
 
-    # Modifying an Attribute's Value Through a Method
-    def update_odometer(self, mileage):
-        """
-        Set the odometer reading to the given value.
-        Reject the change if it attempts to roll the odometer back.
-        """
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
+#     # Modifying an Attribute's Value Through a Method
+#     def update_odometer(self, mileage):
+#         """
+#         Set the odometer reading to the given value.
+#         Reject the change if it attempts to roll the odometer back.
+#         """
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back an odometer!")
 
-    # Incrementing an Attribute's Value Through a Method
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
+#     # Incrementing an Attribute's Value Through a Method
+#     def increment_odometer(self, miles):
+#         """Add the given amount to the odometer reading."""
+#         self.odometer_reading += miles
     
-my_new_car = Car('audi', 'a4', 2024)
-print(my_new_car.get_descriptive_name())
+# my_new_car = Car('audi', 'a4', 2024)
+# print(my_new_car.get_descriptive_name())
 
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
 
-my_used_car = Car('subaru', 'outback', 2019)
-print(my_used_car.get_descriptive_name())
+# my_used_car = Car('subaru', 'outback', 2019)
+# print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
 
 """A set of classes used to represent gas and electric cars."""
 class Car:
@@ -125,54 +125,54 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-# Instances as Attributes
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
-    def __init__(self, battery_size=40):
-        """Initialize the battery's attributes."""
-        self.battery_size = battery_size
+# # Instances as Attributes
+# class Battery:
+#     """A simple attempt to model a battery for an electric car."""
+#     def __init__(self, battery_size=40):
+#         """Initialize the battery's attributes."""
+#         self.battery_size = battery_size
 
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kWh battery.")
 
-    def get_range(self):
-        """Print a statement about the range this battery provides."""
-        if self.battery_size == 40:
-            range = 15
-        elif self.battery_size == 65:
-            range = 25
-        print(f"This car can go about {range} miles on a full charge.")
+#     def get_range(self):
+#         """Print a statement about the range this battery provides."""
+#         if self.battery_size == 40:
+#             range = 15
+#         elif self.battery_size == 65:
+#             range = 25
+#         print(f"This car can go about {range} miles on a full charge.")
 
-    def upgrade_battery(self):
-        """Upgrade the size of the battery."""
-        self.battery_size = 65
-        """Print a statement to show the battery size has upgraded."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+#     def upgrade_battery(self):
+#         """Upgrade the size of the battery."""
+#         self.battery_size = 65
+#         """Print a statement to show the battery size has upgraded."""
+#         print(f"This car has a {self.battery_size}-kWh battery.")
 
 
-class ElectricCar(Car):
-    """Represent aspects of a car, specific to electric vehicles."""
-    def __init__(self, make, model, year):
-        """
-        Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car.
-        """
-        super().__init__(make, model, year)
-        self.battery = Battery()
+# class ElectricCar(Car):
+#     """Represent aspects of a car, specific to electric vehicles."""
+#     def __init__(self, make, model, year):
+#         """
+#         Initialize attributes of the parent class.
+#         Then initialize attributes specific to an electric car.
+#         """
+#         super().__init__(make, model, year)
+#         self.battery = Battery()
 
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kWh battery.")
 
-    # Overriding Methods form the Parent Class
-    def fill_gas_tank(self):
-        """Electric cars don't have gas tanks."""
-        print("This car doesn't have a gas tank!")
+#     # Overriding Methods form the Parent Class
+#     def fill_gas_tank(self):
+#         """Electric cars don't have gas tanks."""
+#         print("This car doesn't have a gas tank!")
 
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
-my_leaf.battery.describe_battery()
-my_leaf.battery.get_range()
-my_leaf.battery.upgrade_battery()
-my_leaf.battery.get_range()
+# my_leaf = ElectricCar('nissan', 'leaf', 2024)
+# print(my_leaf.get_descriptive_name())
+# my_leaf.battery.describe_battery()
+# my_leaf.battery.get_range()
+# my_leaf.battery.upgrade_battery()
+# my_leaf.battery.get_range()
--- Chapter 9 - Classes/my_cars.py
+++ Chapter 9 - Classes/my_cars.py
@@ -1,21 +1,29 @@
-# Importing Multiple Classes from a Module
-from car import Car, ElectricCar
+# # Importing Multiple Classes from a Module
+# from car import Car, ElectricCar
 
-my_mustang = Car('ford', 'mustang', 2024)
-print(my_mustang.get_descriptive_name())
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
+# my_mustang = Car('ford', 'mustang', 2024)
+# print(my_mustang.get_descriptive_name())
+# my_leaf = ElectricCar('nissan', 'leaf', 2024)
+# print(my_leaf.get_descriptive_name())
 
-# Importing an Entire Module
-import car
+# # Importing an Entire Module
+# import car
 
-my_mustang = car.Car('ford', 'mustang', 2024)
-print(my_mustang.get_descriptive_name())
+# my_mustang = car.Car('ford', 'mustang', 2024)
+# print(my_mustang.get_descriptive_name())
 
-my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
+# my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+# print(my_leaf.get_descriptive_name())
 
 # Importing All Classes from a Module - NOT RECOMMENDED
 # from module_name import *
 
 # Importing a Module into a Module
+from car import Car
+from electric_car import ElectricCar
+
+my_mustang = Car('ford', 'mustang', 2024)
+print(my_mustang.get_descriptive_name())
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
