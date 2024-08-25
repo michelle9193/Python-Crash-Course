0bde5d1f20a1b07eceafa4af1bdfa90f5ca70027
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 21:02:36 +0200

    add exercise 9-8

--- Try It Yourself/9_test.py
+++ Try It Yourself/9_test.py
@@ -262,7 +262,59 @@
 # spur.describe_restaurant()
 # spur.show_flavors()
 
-# 9-7 Admin
+# # 9-7 Admin
+# class Users:
+#     """This is how to add users"""
+
+#     def __init__(self, first_name, last_name, email, location, username):
+#         """Initialize a user"""
+#         self.first_name = first_name.title()
+#         self.last_name = last_name.title()
+#         self.email = email
+#         self.location = location.title()
+#         self.username = username
+
+#     def describe_user(self):
+#         """Print a summary of the user's information"""
+#         # summary = f"First Name: {self.first_name}\nLast Name: {self.last_name}
+#         # \nEmail: {self.email}\nLocation: {self.location}\nUsername: 
+#         # {self.username}"
+#         # print(f"\n{summary}")
+#         print(f"\n{self.first_name} {self.last_name}")
+#         print(f"{self.email}")
+#         print(f"{self.location}")
+#         print(f"{self.username}")
+
+#     def greet_user(self):
+#         """A personalize greeting to the user"""
+#         greeting = f"Hello, {self.first_name} {self.last_name}!"
+#         print(f"\n{greeting}")
+
+# class Admin(Users):
+#     """Initialize admin privileges."""
+#     def __init__(self, first_name, last_name, email, location, username):
+#         """
+#         Initialize attributes of the parent class.
+#         Then initiliaze attributes specific to an administrator.
+#         """
+#         super().__init__(first_name, last_name, email, location, username)
+#         self.privileges = []
+    
+#     def show_privileges(self):
+#         """Display all the admin privileges."""
+#         print("The admin has the following privileges:")
+#         for privilege in self.privileges:
+#             print(f"- {privilege}")
+
+# # Make an instance called users from the class
+# admin = Admin('billy', 'james', 'billy@hotmail.com', 'claremont', 
+# 'admin_billy')
+# admin.privileges = [ ]
+
+# admin.describe_user()
+# admin.show_privileges()
+
+# 9-8 Privileges
 class Users:
     """This is how to add users"""
 
@@ -290,25 +342,46 @@ class Users:
         greeting = f"Hello, {self.first_name} {self.last_name}!"
         print(f"\n{greeting}")
 
+class Privileges:
+    """Model privileges to the admin user."""
+    def __init__(self, privileges=[]):
+        """Initialize privileges attribute"""
+        self.privileges = privileges
+
+    def show_privileges(self):
+        """Display all the admin privileges."""
+        print(f"The admin has the following privileges: {self.privileges}")
+        if self.privileges:
+            for privilege in self.privileges:
+                print(f"- {privilege}")
+        else:
+            print("- This user has no privileges.")
+
 class Admin(Users):
-    """Initialize admin privileges."""
+    """Initialize admin priexcvileges."""
     def __init__(self, first_name, last_name, email, location, username):
         """
         Initialize attributes of the parent class.
         Then initiliaze attributes specific to an administrator.
         """
         super().__init__(first_name, last_name, email, location, username)
-        self.privileges = []
+        
+        # Initialize an empty set of privileges.
+        self.privileges = Privileges()
     
-    def show_privileges(self):
-        """Display all the admin privileges."""
-        print("The admin has the following privileges:")
-        for privilege in self.privileges:
-            print(f"- {privilege}")
-
 # Make an instance called users from the class
-admin = Admin('billy', 'james', 'billy@hotmail.com', 'claremont', 'admin_billy')
-admin.privileges = ['can add post', 'can delete post', 'can ban user' ]
+billy = Admin('billy', 'james', 'billy@hotmail.com', 'claremont', 
+                        'admin_billy')
+billy.describe_user
 
-admin.describe_user()
-admin.show_privileges()
+billy.privileges.show_privileges()
+
+print("\nAdding privileges:")
+billy_privileges = [
+    'can add post', 
+    'can delete post', 
+    'can ban user'
+    ]
+
+billy.privileges.privileges = billy_privileges
+billy.privileges.show_privileges()
