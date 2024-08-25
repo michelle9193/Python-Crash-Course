1ca69b58929d726f49d0d8d2dd51569bba3712d4
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:54:40 +0200

    add exercise 9-11

--- Try It Yourself/9_test.py
+++ Try It Yourself/9_test.py
@@ -468,3 +468,8 @@
 
 # 9-10 Imported Restaurant
 # Create separate file that imports Restaurant (imported_restaurant.py)
+
+# 9-11 Imported Admin
+# Start with your work from Exercise 9-8. Store the classes User, Privileges, 
+# and Admin in one module. Create a separate file, make an Admin instance, and 
+# call show_privileges() to show that everything is working correctly.
