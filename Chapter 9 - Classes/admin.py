aa5ae08f594d88dbab3d8838c4367c340b88bc77
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 23:02:11 +0200

    update

--- Chapter 9 - Classes/admin.py
+++ Chapter 9 - Classes/admin.py
@@ -1,6 +1,25 @@
-from imported_admin import Admin
+# from imported_admin import Admin
 
-# Make an instance called users from the class
+# # Make an instance called users from the class
+# billy = Admin('billy', 'james', 'billy@hotmail.com', 'claremont', 
+#                         'admin_billy')
+# billy.describe_user
+
+# billy.privileges.show_privileges()
+
+# print("\nAdding privileges:")
+# billy_privileges = [
+#     'can add post', 
+#     'can delete post', 
+#     'can ban user'
+#     ]
+
+# billy.privileges.privileges = billy_privileges
+# billy.privileges.show_privileges()
+
+from user import Users
+from privileges_admin import Privileges, Admin
+
 billy = Admin('billy', 'james', 'billy@hotmail.com', 'claremont', 
                         'admin_billy')
 billy.describe_user
