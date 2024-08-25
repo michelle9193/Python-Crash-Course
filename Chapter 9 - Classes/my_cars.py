bc38438f40d688aacee680fe9e54e5585538636d
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:38:03 +0200

    update

--- Chapter 9 - Classes/my_cars.py
+++ Chapter 9 - Classes/my_cars.py
@@ -27,3 +27,13 @@ print(my_mustang.get_descriptive_name())
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+
+# Using Aliases
+from electric_car import ElectricCar as EC
+my_leaf = EC('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+
+# Giving a module an alias
+import electric_car as ec
+my_leaf = ec.ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
