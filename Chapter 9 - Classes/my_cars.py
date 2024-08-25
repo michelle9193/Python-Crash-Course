e96a532933f0195c06bc5485792c3baad0116ac2
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:22:38 +0200

    update

--- Chapter 9 - Classes/my_cars.py
+++ Chapter 9 - Classes/my_cars.py
@@ -5,3 +5,17 @@ my_mustang = Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+
+# Importing an Entire Module
+import car
+
+my_mustang = car.Car('ford', 'mustang', 2024)
+print(my_mustang.get_descriptive_name())
+
+my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+
+# Importing All Classes from a Module - NOT RECOMMENDED
+# from module_name import *
+
+# Importing a Module into a Module
