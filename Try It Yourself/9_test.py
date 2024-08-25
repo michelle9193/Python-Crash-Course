04d8b1b90acee1d8ba05af1e2b2d67ec9c3d9074
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:56:43 +0200

    add exercise 9-12

--- Try It Yourself/9_test.py
+++ Try It Yourself/9_test.py
@@ -473,3 +473,8 @@
 # Start with your work from Exercise 9-8. Store the classes User, Privileges, 
 # and Admin in one module. Create a separate file, make an Admin instance, and 
 # call show_privileges() to show that everything is working correctly.
+
+# 9-12 Multiple Modules
+# Store the User class in one module, and store the Privileges and Admin classes
+# in a separate module. In a separate file, create an Admin instance and call 
+# show_privileges() to show that everything is still working correctly.
