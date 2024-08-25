857a54f124a985a0a7641fabb65b4bc34bf2b564
michelle9193 <michellefortuin93@gmail.com>
2024-08-25 22:08:26 +0200

    update

--- Try It Yourself/9_test.py
+++ Try It Yourself/9_test.py
@@ -386,58 +386,58 @@
 # billy.privileges.privileges = billy_privileges
 # billy.privileges.show_privileges()
 
-# # 9-9 Battery Update
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#         self.odometer_reading = 0
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model}"
-#         return long_name.title()
+# 9-9 Battery Update
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
 
-#     def read_odometer(self):
-#         """Print a statement showing the car's mileage."""
-#         print(f"This car has {self.odometer_reading} miles on it.")
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-#     def update_odometer(self, mileage):
-#         """Set the odometer reading to the given value."""
-#         if mileage >= self.odometer_reading:
-#             self.odometer_reading = mileage
-#         else:
-#             print("You can't roll back an odometer!")
-#     def increment_odometer(self, miles):
-#         """Add the given amount to the odometer reading."""
-#         self.odometer_reading += miles
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
 
-# # Instances as Attributes
-# class Battery:
-#     """A simple attempt to model a battery for an electric car."""
-#     def __init__(self, battery_size=40):
-#         """Initialize the battery's attributes."""
-#         self.battery_size = battery_size
+# Instances as Attributes
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
 
-#     def describe_battery(self):
-#         """Print a statement describing the battery size."""
-#         print(f"This car has a {self.battery_size}-kWh battery.")
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
-#     def get_range(self):
-#         """Print a statement about the range this battery provides."""
-#         if self.battery_size == 40:
-#             range = 15
-#         elif self.battery_size == 65:
-#             range = 25
-#         print(f"This car can go about {range} miles on a full charge.")
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 15
+        elif self.battery_size == 65:
+            range = 25
+        print(f"This car can go about {range} miles on a full charge.")
 
-#     def upgrade_battery(self):
-#         """Upgrade the size of the battery."""
-#         self.battery_size = 65
-#         """Print a statement to show the battery size has upgraded."""
-#         print(f"This car has a {self.battery_size}-kWh battery.")
+    def upgrade_battery(self):
+        """Upgrade the size of the battery."""
+        self.battery_size = 65
+        """Print a statement to show the battery size has upgraded."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 
 class ElectricCar(Car):
