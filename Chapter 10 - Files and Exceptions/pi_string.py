1b539f8337cb76666d6d88dac75606e1d987785a
michelle9193 <michellefortuin93@gmail.com>
2024-08-31 21:31:58 +0200

    add file

---
+++ Chapter 10 - Files and Exceptions/pi_string.py
@@ -0,0 +1,10 @@
+# # Making a List of Lines from a File
+# filename = 'pi_digits.txt'
+
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+
+# for line in lines:
+#     print(line.rstrip())
+
+# Working with a File's Contents
