99ed12c1cd07f7510e56cbb27fe6a08207d8ac98
michelle9193 <michellefortuin93@gmail.com>
2024-08-31 21:40:01 +0200

    update

--- Chapter 10 - Files and Exceptions/pi_string.py
+++ Chapter 10 - Files and Exceptions/pi_string.py
@@ -11,3 +11,15 @@
 # print(pi_string)
 # print(len(pi_string))
 
+# Large Files: One Million Digits
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string[:52] + "...")
+print(len(pi_string))
