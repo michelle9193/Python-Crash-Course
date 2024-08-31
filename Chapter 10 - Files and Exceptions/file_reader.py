7d058ce34ba945d32dbe1aac39ae888246ccb253
michelle9193 <michellefortuin93@gmail.com>
2024-08-31 21:30:06 +0200

    update

--- Chapter 10 - Files and Exceptions/file_reader.py
+++ Chapter 10 - Files and Exceptions/file_reader.py
@@ -3,9 +3,19 @@
 #     contents = file_object.read()
 #     print(contents.rstrip())
 
-# Reading Line by Line
-filename = 'pi_digits.txt'
+# # Reading Line by Line
+# filename = 'pi_digits.txt'
 
-with open(filename) as file_object:
-    for line in file_object:
-        print(line)
+# with open(filename) as file_object:
+#     for line in file_object:
+#         print(line.rstrip())
+
+# # Making a List of Lines from a File
+# filename = 'pi_digits.txt'
+
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+
+# for line in lines:
+#     print(line.rstrip())
+
