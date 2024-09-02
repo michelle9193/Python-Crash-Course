44de278a334bdf20e7daf826bbf2b12a866bbe7d
michelle9193 <michellefortuin93@gmail.com>
2024-09-02 22:40:52 +0200

    add file

--- Chapter 10 - Files and Exceptions/write_message.py
+++ Chapter 10 - Files and Exceptions/write_message.py
@@ -1,6 +1,16 @@
-# Writing to a File
-# Writing a Single Line
+# # Writing to a File
+# # Writing a Single Line
+# from pathlib import Path
+
+# path = Path('programming.txt')
+# path.write_text("I love programming!")
+
+# Writing Multiple Lines
 from pathlib import Path
 
+contents = "I love programming.\n"
+contents += "I love creating new games.\n"
+contents += "I also love working with data.\n"
+
 path = Path('programming.txt')
-path.write_text("I love programming!")
+path.write_text(contents)
