f4b8dab550bf14e5482f42ccbfc2bcfdec4e5f9e
michelle9193 <michellefortuin93@gmail.com>
2024-09-02 19:56:49 +0200

    add exercise 10-1

--- Try It Yourself/10_test.py
+++ Try It Yourself/10_test.py
@@ -1,1 +1,20 @@
-# 10-1 Learning Python
+# # 10-1 Learning Python
+# # Write a program that reads the file and prints what you wrote.
+# filename = 'learning_python.txt'
+
+# print("Reading an entire file:")
+# with open(filename) as file_object:
+#     contents = file_object.read()
+# print(contents)
+
+# print("\nLooping over the lines:")
+# with open(filename) as file_object:
+#     for line in file_object:
+#         print(line.strip())
+
+# print("\nStoring the lines in a list")
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+
+# for line in lines:
+#     print(line.rstrip())
