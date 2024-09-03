aa8ff685dee1cb35ff2536add5751a3a31e4940e
michelle9193 <michellefortuin93@gmail.com>
2024-09-03 17:08:30 +0200

    update

--- Chapter 10 - Files and Exceptions/division_calculator.py
+++ Chapter 10 - Files and Exceptions/division_calculator.py
@@ -1,3 +1,23 @@
 # Exceptions
 # Handling the ZeroDivisionError Exception
-print(5/0)
+# print(5/0)
+
+# # Using try-except Blocks
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+
+# Using Excpetions to Prevent Crashes
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    answer = int(first_number) / int(second_number)
+    print(answer)
