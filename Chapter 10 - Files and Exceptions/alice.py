21ba4824fddc9859ccedb8b0f6967ec076ff92f5
michelle9193 <michellefortuin93@gmail.com>
2024-09-04 10:25:41 +0200

    update

--- Chapter 10 - Files and Exceptions/alice.py
+++ Chapter 10 - Files and Exceptions/alice.py
@@ -6,3 +6,9 @@ try:
     contents = path.read_text(encoding='utf-8')
 except FileNotFoundError:
     print(f"Sorry, the file {path} does not exist.")
+# Analyzing Text
+else:
+    # Count the approximate number of words in the file:
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
