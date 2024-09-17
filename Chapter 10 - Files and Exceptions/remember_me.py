3029fee5ad4c267c817f1c73fa7e4710f803b3df
michellefortuin93 <michellefortuin93@gmail.com>
2024-09-17 21:49:41 +0200

    update

--- Chapter 10 - Files and Exceptions/remember_me.py
+++ Chapter 10 - Files and Exceptions/remember_me.py
@@ -10,17 +10,36 @@
 
 # print(f"We'll remember you when you come back, {username}!")
 
-# Combine json.loads() and json.dumps() programs into one file.
+# # Combine json.loads() and json.dumps() programs into one file.
+# from pathlib import Path
+# import json
+
+# path = Path('username.json')
+# if path.exists():
+#     contents = path.read_text()
+#     username = json.loads(contents)
+#     print(f"Welcome back, {username}!")
+# else:
+#     username = input("What is your name? ")
+#     contents = json.dumps(username)
+#     path.write_text(contents)
+#     print(f"We'll remember you when you come back, {username}!")
+
+# Refactoring
 from pathlib import Path
 import json
 
-path = Path('username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("What is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"We'll remember you when you come back, {username}!")
+def greet_user():
+    """Greet the user by name."""
+    path = Path('username.json')
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
