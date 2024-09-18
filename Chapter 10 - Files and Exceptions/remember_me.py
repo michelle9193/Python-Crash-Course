3eb46360e8c6405ded4a8f6947f58d6e618d7ee1
michellefortuin93 <michellefortuin93@gmail.com>
2024-09-18 17:01:48 +0200

    update

--- Chapter 10 - Files and Exceptions/remember_me.py
+++ Chapter 10 - Files and Exceptions/remember_me.py
@@ -25,21 +25,84 @@
 #     path.write_text(contents)
 #     print(f"We'll remember you when you come back, {username}!")
 
-# Refactoring
+# # Refactoring
+# from pathlib import Path
+# import json
+
+# def greet_user():
+#     """Greet the user by name."""
+#     path = Path('username.json')
+#     if path.exists():
+#         contents = path.read_text()
+#         username = json.loads(contents)
+#         print(f"Welcome back, {username}!")
+#     else:
+#         username = input("What is your name? ")
+#         contents = json.dumps(username)
+#         path.write_text(contents)
+#         print(f"We'll remember you when you come back, {username}!")
+
+# greet_user()
+
+# # Refactor greet_user() so it’s not doing so many different tasks. 
+# # Start by moving the code for retrieving a stored username to a separate 
+# # function
+# from pathlib import Path
+# import json
+
+# def get_stored_username(path):
+#     """Get stored username if available."""
+#     if path.exists():
+#         contents = path.read_text()
+#         username = json.loads(contents)
+#         return username
+#     else:
+#         return None
+
+# def greet_user():
+#     """Greet the user by name."""
+#     path = Path('username.json')
+#     username = get_stored_username(path)
+#     if username:
+#         print(f"Welcome back, {username}!")
+#     else:
+#         username = input("What is your name? ")
+#         contents = json.dumps(username)
+#         path.write_text(contents)
+#         print(f"We'll remember you when you come back, {username}!")
+
+# greet_user()
+
+# Factor in one more block of code out of greet_user(). If the username doesn't
+# exist, the code that prompts for a new username should be moved to a function 
+# dedicated to that purpose:
 from pathlib import Path
 import json
+
+def get_stored_username(path):
+    """Get stored username if available."""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+
+def get_new_username(path):
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
 
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
-    if path.exists():
-        contents = path.read_text()
-        username = json.loads(contents)
+    username = get_stored_username(path)
+    if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        contents = json.dumps(username)
-        path.write_text(contents)
+        username = get_new_username(path)
         print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
