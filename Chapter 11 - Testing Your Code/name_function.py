edc23df35e43347bd6afa57d0c101d3d288b9478
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-01 17:22:14 +0200

    update

--- Chapter 11 - Testing Your Code/name_function.py
+++ Chapter 11 - Testing Your Code/name_function.py
@@ -5,7 +5,7 @@
 #     return full_name.title()
 
 # # A Failing Test
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     """Generate a neatly formatted full name."""
     full_name = f"{first} {middle} {last}"
     return full_name.title()
