8a39d3ffaddf5ff78dc857ce51d49dcc81ece2b5
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-01 17:18:59 +0200

    update

--- Chapter 11 - Testing Your Code/test_name_function.py
+++ Chapter 11 - Testing Your Code/test_name_function.py
@@ -1,6 +1,7 @@
+# A Passing Test
 from name_function import get_formatted_name
 
 def test_first_last_name():
     """Do names like 'Jannis Joplin' work?"""
-    formatted_name = get_formatted_name('janis', 'joplin')
+    formatted_name = get_formatted_name('janis','marie', 'joplin')
     assert formatted_name == 'Janis Joplin'
