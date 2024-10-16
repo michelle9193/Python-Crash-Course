fa287d8db43aec861b0bf93be33ba468a993b8de
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-16 17:15:02 +0200

    update

--- Chapter 11 - Testing Your Code/language_survey.py
+++ Chapter 11 - Testing Your Code/language_survey.py
@@ -1,4 +1,23 @@
 # Test if AnonymousSurvey class works
+# from survey import AnonymousSurvey
+
+# # Define a question, and make a survey.
+# question = "What language did you first learn to speak?"
+# language_survey = AnonymousSurvey(question)
+
+# # Show the question, and store responses to the question.
+# lanuage_survey.show_question()
+# print("Enter 'q' at any time to quit./n")
+# while True:
+#     response = input("Language: ")
+#     if response == 'q':
+#         break
+#     language_survey.store_response(response)
+
+# # Show the survey results.
+# print("\nThank you to everyone who participated in the survey!")
+# language_survey.show_results()
+
 from survey import AnonymousSurvey
 
 # Define a question, and make a survey.
@@ -6,8 +25,8 @@ question = "What language did you first learn to speak?"
 language_survey = AnonymousSurvey(question)
 
 # Show the question, and store responses to the question.
-lanuage_survey.show_question()
-print("Enter 'q' at any time to quit./n")
+language_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
 while True:
     response = input("Language: ")
     if response == 'q':
