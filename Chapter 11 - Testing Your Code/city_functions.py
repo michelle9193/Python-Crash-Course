eb267645a470ded2d263ca85fc3f83df7608f83b
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-16 11:33:43 +0200

    update

--- Chapter 11 - Testing Your Code/city_functions.py
+++ Chapter 11 - Testing Your Code/city_functions.py
@@ -1,21 +1,22 @@
 # 11-1 City, Country
-# def get_formatted_name(city, country):
+# def city_country(city, country):
 #     """Generate a City and Country."""
 #     city_country = f"{city}, {country}"
 #     return city_country.title() 
 
 # 11-2 Population
 # Modify your function so it requires a third parameter, population.
-def get_formatted_name(city, country, population):
+def city_country(city, country, population):
     """Return a string of city, country - population 5_000_000."""
     output_string = f"{city.title()}, {country.title()}"
     output_string += f" -population {population}"
     return output_string
 
-# def get_formatted_name(city, country, population='5000000'):
-#     """Generate a City, Country and population."""
-#     if population:
-#         city_country = f"{city.title()}, {country.title()} - population: {population}"
-#     else:
-#         city_country = f"{city.title()}, {country.title()}"
-#     return city_country
+# City Functions Optional
+def city_country(city, country, population='5000000'):
+    """Generate a City, Country and population."""
+    if population:
+        city_country = f"{city.title()}, {country.title()} - population: {population}"
+    else:
+        city_country = f"{city.title()}, {country.title()}"
+    return city_country
--- Chapter 11 - Testing Your Code/test_cities.py
+++ Chapter 11 - Testing Your Code/test_cities.py
@@ -1,15 +1,15 @@
-from city_functions import get_formatted_name as gfn
+from city_functions import city_country
 
-# Failed test
+# # Failed test
 def test_city_country():
     """Does a simple city and country pair work?"""
-    santiago_chile = output_string('santiago', 'chile')
+    santiago_chile = city_country('santiago', 'chile')
     assert santiago_chile == 'Santiago, Chile'
 
-# def test_city_country():
-#     """Populate a City and Country."""
-#     formatted_name = gfn('santiago', 'chile', '5000000')
-#     assert formatted_name == 'Santiago, Chile - population: 5000000'
+def test_city_country():
+    """Populate a City and Country."""
+    formatted_name = city_country('santiago', 'chile', '5000000')
+    assert formatted_name == 'Santiago, Chile - population: 5000000'
 
 # def test_city_country_population:
 #     """Verify """
