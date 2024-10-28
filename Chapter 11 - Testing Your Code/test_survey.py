c597e70838682b0c7a315320593020bf56a64da1
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-28 20:42:33 +0200

    update

--- Chapter 11 - Testing Your Code/test_survey.py
+++ Chapter 11 - Testing Your Code/test_survey.py
@@ -1,4 +1,34 @@
+# from survey import AnonymousSurvey
+
+# def test_store_single_response():
+#     """Test that a single response is stored properly."""
+#     question = "What language did you first learn to speak?"
+#     language_survey = AnonymousSurvey(question)
+#     language_survey.store_response('English')
+#     assert 'English' in language_survey.responses
+
+# # Verify if more than one response can be stored correctly.
+# def test_store_three_responses():
+#     """Test that three individual responses are stored properly."""
+#     question = "What langauges did you first learn to speak?"
+#     language_survey = AnonymousSurvey(question)
+#     responses = ['English', 'Spanish', 'Mandarin']
+#     for response in responses:
+#         language_survey.store_response(response)
+
+#     for response in responses:
+#         assert response in language_survey.responses
+
+# Using Fixtures
+import pytest
 from survey import AnonymousSurvey
+
+@pytest.fixture
+def language_survey():
+    """A survey that will be available to all test functions."""
+    question = "What language did you first learn to speak?"
+    language_survey = AnonymousSurvey(question)
+    return language_survey 
 
 def test_store_single_response():
     """Test that a single response is stored properly."""
