61cfab15f9893d385e38e9268872d843acbc0449
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-28 22:11:52 +0200

    update

--- Chapter 11 - Testing Your Code/employee.py
+++ Chapter 11 - Testing Your Code/employee.py
@@ -2,13 +2,12 @@
 class Employee:
     """Determine how much increase an employee receives."""
 
-    def __init__(self, first_name, last_name, annual_salary):
+    def __init__(self, first_name, last_name, salary):
         """Store employee information."""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.annual_salary = input("Please enter your annual salary")
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.salary = salary
 
-    def give_raise(self, default_value, custom_raise):
-        """Determine how much the raise amount is."""
-        self.default_value = 5000
-        self.custom_raise = input("Enter custom value")
+    def give_raise(self, amount=5000):
+        """Give employee a raise."""
+        self.salary += amount
--- Chapter 11 - Testing Your Code/test_employee.py
+++ Chapter 11 - Testing Your Code/test_employee.py
@@ -1,15 +1,32 @@
+# from employee import Employee
+
+# def test_give_defaul_raise():
+#     """Test that a default raise works correctly."""
+#     employee = Employee('lesley', 'white', 50_000)
+#     employee.give_raise()
+#     assert employee.salary == 55_000
+
+# def test_give_custom_raise():
+#     """Test that a custom raise works correctly."""
+#     employee = Employee('lesley', 'white', 50_000)
+#     employee.give_raise(10000)
+#     assert employee.salary == 60_000
+
+import pytest
 from employee import Employee
 
-# def test_store_employee():
-#     """Gather employee information."""
-#     first_name = "What is your first name? "
-#     last = "What is your last name? "
-#     annual salary = "What is your annual salary? "
+@pytest.fixture
+def employee():
+    """An Employee object that will be available to all test functions."""
+    employee = Employee('eric', 'matthes', 65_000)
+    return employee
 
-def test_give_default():
-    """Add $5000 dollars to the annual salary."""
-    default = input("Press '1' to confirm the default raise.")
-    while True: 
-        response = input("Would you like to use the default raise? y/n")
-    if response == 'y':
-        amount = int(annual_salary) + int(5000)
+def test_give_default_raise(employee):
+    """Test that a default raise works correctly."""
+    employee.give_raise()
+    assert employee.salary == 70_000
+
+def test_give_custom_raise(employee):
+    """Test that a custom raise works correctly."""
+    employee.give_raise(10000)
+    assert employee.salary == 75_000
