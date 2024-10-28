afb0ff5ba812d4927a5c348d9fe5c6be7ce1cee0
michellefortuin93 <michellefortuin93@gmail.com>
2024-10-28 20:49:29 +0200

    update

--- Chapter 11 - Testing Your Code/test_survey.py
+++ Chapter 11 - Testing Your Code/test_survey.py
@@ -30,18 +30,14 @@ def language_survey():
     language_survey = AnonymousSurvey(question)
     return language_survey 
 
-def test_store_single_response():
+def test_store_single_response(language_survey):
     """Test that a single response is stored properly."""
-    question = "What language did you first learn to speak?"
-    language_survey = AnonymousSurvey(question)
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
 
 # Verify if more than one response can be stored correctly.
-def test_store_three_responses():
+def test_store_three_responses(language_survey):
     """Test that three individual responses are stored properly."""
-    question = "What langauges did you first learn to speak?"
-    language_survey = AnonymousSurvey(question)
     responses = ['English', 'Spanish', 'Mandarin']
     for response in responses:
         language_survey.store_response(response)
